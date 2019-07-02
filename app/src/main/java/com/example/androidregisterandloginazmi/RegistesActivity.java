fffbce7d0f79c0117d65a1d3e7c5acf9556cab72
azmirohim <azmirohim0301@gmail.com>
2019-07-02 20:21:38 +0700

    Initial commit

---
+++ app/src/main/java/com/example/androidregisterandloginazmi/RegistesActivity.java
@@ -0,0 +1,13 @@
+package com.example.androidregisterandloginazmi;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RegistesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registes);
+    }
+}
